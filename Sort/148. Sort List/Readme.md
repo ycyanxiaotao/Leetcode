c890c257a4b71dbeb03177c0a31fb7e1bd7ab4bb
Xiaotao Yan <ycyanxiaotao@gmail.com>
2023-12-03 11:46:55 -0800

    first commit

---
+++ Sort/148. Sort List/Readme.md
@@ -0,0 +1,6 @@
+### 148. Sort List
+
+分治 merge sort,快慢指针找中点，中点前一个点的next置null
+
+
+[Leetcode Link](https://leetcode.com/problems/sort-list/)
---
+++ Sort/148. Sort List/Solution.java
@@ -0,0 +1,47 @@
+
+public class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode prev = null, slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        prev.next = null;
+        ListNode l1 = sortList(head);
+        ListNode l2 = sortList(slow);
+        ListNode res = mergeLists(l1, l2);
+        return res;
+    }
+
+    private ListNode mergeLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        ListNode prev = new ListNode(-1), q = prev;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                prev.next = l1;
+                l1 = l1.next;
+            } else {
+                prev.next = l2;
+                l2 = l2.next;
+            }
+            prev = prev.next;
+
+        }
+        if (l1 != null) {
+            prev.next = l1;
+        }
+        if (l2 != null) {
+            prev.next = l2;
+        }
+        return q.next;
+    }
+}
